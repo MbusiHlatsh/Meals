f067b2b89761e98455618e426623b8267677bdd9
Mbusi Hlatshwayo - vn56qqx <Mbusi.Hlatshwayo1@walmart.com>
2024-01-30 06:40:13 -0800

    clean up code

--- Meals/Sources/Constants/MealConstants.swift
+++ Meals/Sources/Constants/MealConstants.swift
@@ -8,18 +8,19 @@
 import Foundation
 
 struct Constants {
+    // Network layer constants
     static let dessertEndpoint = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
     static let mealEndpoint = "https://themealdb.com/api/json/v1/1/lookup.php?i="
 
+    // View layer constants
     static let mealViewTitle = "Desserts"
     static let genericErrorMessage = "Sorry there was an issue"
     static let genericErrorDismissMessage = "OK"
     static let placeholderImage = "placeholderImage"
-
     static let mealCollectionViewCellID = "MealCollectionViewCell"
-
     static let invalidURLMessage = "Invalid URL"
     
+    // Model layer constants
     static let mealDetailsTitleKey = "strMeal"
     static let mealDetailsIngredientKey = "strIngredient"
     static let mealDetailsMeasurementKey = "strMeasure"
